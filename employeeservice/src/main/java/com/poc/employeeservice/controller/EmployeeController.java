a46e3ec6e0c549bdc69aa1f55a0c085776c8197b
vinay <rvinayk.4444@gmail.com>
2019-05-22 22:16:56 -0400

    Added controller

---
+++ employeeservice/src/main/java/com/poc/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.poc.employeeservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	
+	@GetMapping("/test")
+	public String test() {
+		
+		return "test";
+	}
+
+}
